631950abbf812609c7d1c0cef94deb0c5e63a507
Michael Primm <mprimm@mirificsystems.com>
2023-05-02 18:06:41 -0500

    Bump to 0.3.0 - switch to class for Region, update endpoints, and allow custom values

--- api/src/main/java/io/github/linktosriram/s3lite/api/region/Region.java
+++ api/src/main/java/io/github/linktosriram/s3lite/api/region/Region.java
@@ -1,6 +1,7 @@
 package io.github.linktosriram.s3lite.api.region;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.stream.Stream;
 
 import static java.net.URI.create;
@@ -8,62 +9,82 @@ import static java.net.URI.create;
 /**
  * Reference: https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
  */
-public enum Region {
+public class Region {
+	private static HashMap<String, Region> regionsByName = new HashMap<String, Region>();
 
-    US_EAST_1("us-east-1", create("https://s3.us-east-1.amazonaws.com")),
-    US_EAST_2("us-east-2", create("https://s3.us-east-2.amazonaws.com")),
-    US_WEST_1("us-west-1", create("https://s3.us-west-1.amazonaws.com")),
-    US_WEST_2("us-west-2", create("https://s3.us-west-2.amazonaws.com")),
+	public static Region US_EAST_1 = new Region("us-east-1", create("https://s3.us-east-1.amazonaws.com")),
+			US_EAST_2 = new Region("us-east-2", create("https://s3.us-east-2.amazonaws.com")),
+			US_WEST_1 = new Region("us-west-1", create("https://s3.us-west-1.amazonaws.com")),
+			US_WEST_2 = new Region("us-west-2", create("https://s3.us-west-2.amazonaws.com")),
 
-    AP_EAST("ap-east", create("https://s3.ap-east-1.amazonaws.com")),
-    AP_SOUTH_1("ap-south-1", create("https://s3.ap-south-1.amazonaws.com")),
-    AP_NORTHEAST_1("ap-northeast-1", create("https://s3.ap-northeast-1.amazonaws.com")),
-    AP_NORTHEAST_2("ap-northeast-2", create("https://s3.ap-northeast-2.amazonaws.com")),
-    AP_NORTHEAST_3("ap-northeast-3", create("https://s3.ap-northeast-3.amazonaws.com")),
-    AP_SOUTHEAST_1("ap-southeast-1", create("https://s3.ap-southeast-1.amazonaws.com")),
-    AP_SOUTHEAST_2("ap-southeast-2", create("https://s3.ap-southeast-2.amazonaws.com")),
+			AF_SOUTH_1 = new Region("af-south-1", create("https://s3.af-south-1.amazonaws.com")),
+			
+			AP_EAST_1 = new Region("ap-east-1", create("https://s3.ap-east-1.amazonaws.com")),
+			AP_SOUTH_1 = new Region("ap-south-1", create("https://s3.ap-south-1.amazonaws.com")),
+			AP_SOUTH_2 = new Region("ap-south-2", create("https://s3.ap-south-2.amazonaws.com")),
+			AP_NORTHEAST_1 = new Region("ap-northeast-1", create("https://s3.ap-northeast-1.amazonaws.com")),
+			AP_NORTHEAST_2 = new Region("ap-northeast-2", create("https://s3.ap-northeast-2.amazonaws.com")),
+			AP_NORTHEAST_3 = new Region("ap-northeast-3", create("https://s3.ap-northeast-3.amazonaws.com")),
+			AP_SOUTHEAST_1 = new Region("ap-southeast-1", create("https://s3.ap-southeast-1.amazonaws.com")),
+			AP_SOUTHEAST_2  = new Region("ap-southeast-2", create("https://s3.ap-southeast-2.amazonaws.com")),
+			AP_SOUTHEAST_3 = new Region("ap-southeast-3", create("https://s3.ap-southeast-3.amazonaws.com")),
+			AP_SOUTHEAST_4  = new Region("ap-southeast-4", create("https://s3.ap-southeast-4.amazonaws.com")),
 
-    EU_WEST_1("eu-west-1", create("https://s3.eu-west-1.amazonaws.com")),
-    EU_WEST_2("eu-west-2", create("https://s3.eu-west-2.amazonaws.com")),
-    EU_WEST_3("eu-west-3", create("https://s3.eu-west-3.amazonaws.com")),
-    EU_CENTRAL_1("eu-central-1", create("https://s3.eu-central-1.amazonaws.com")),
-    EU_NORTH_1("eu-north-1", create("https://s3.eu-north-1.amazonaws.com")),
+			EU_WEST_1 = new Region("eu-west-1", create("https://s3.eu-west-1.amazonaws.com")),
+			EU_WEST_2 = new Region("eu-west-2", create("https://s3.eu-west-2.amazonaws.com")),
+			EU_WEST_3 = new Region("eu-west-3", create("https://s3.eu-west-3.amazonaws.com")),
+			EU_CENTRAL_1 = new Region("eu-central-1", create("https://s3.eu-central-1.amazonaws.com")),
+			EU_CENTRAL_2 = new Region("eu-central-2", create("https://s3.eu-central-2.amazonaws.com")),
+			EU_NORTH_1 = new Region("eu-north-1", create("https://s3.eu-north-1.amazonaws.com")),
+			EU_SOUTH_1 = new Region("eu-south-1", create("https://s3.eu-south-1.amazonaws.com")),
+			EU_SOUTH_2 = new Region("eu-south-2", create("https://s3.eu-south-2.amazonaws.com")),
 
-    CA_CENTRAL_1("ca-central-1", create("https://s3.ca-central-1.amazonaws.com")),
+			ME_CENTRAL_1 = new Region("me-central-1", create("https://s3.me-central-1.amazonaws.com")),
+			ME_SOUTH_1 = new Region("me-south-1", create("https://s3.me-south-1.amazonaws.com")),
+			
+			CA_CENTRAL_1 = new Region("ca-central-1", create("https://s3.ca-central-1.amazonaws.com")),
 
-    SA_EAST_1("sa-east-1", create("https://s3.sa-east-1.amazonaws.com")),
+			SA_EAST_1 = new Region("sa-east-1", create("https://s3.sa-east-1.amazonaws.com")),
+			
+			US_GOV_EAST_1 = new Region("us-gov-east-1", create("https://s3.us-gov-east-1.amazonaws.com")),
+			US_GOV_WEST_1 = new Region("us-gov-west-1", create("https://s3.us-gov-west-1.amazonaws.com")),
 
-    CN_NORTH_1("cn-north-1", create("https://s3.cn-north-1.amazonaws.com.cn")),
-    CN_NORTHWEST_1("cn-northwest-1", create("https://s3.cn-northwest-1.amazonaws.com.cn"));
+			CN_NORTH_1 = new Region("cn-north-1", create("https://s3.cn-north-1.amazonaws.com.cn")),
+			CN_NORTHWEST_1 = new Region("cn-northwest-1", create("https://s3.cn-northwest-1.amazonaws.com.cn"));
 
-    private final String regionName;
-    private final URI endpoint;
+	private final String regionName;
+	private final URI endpoint;
 
-    Region(final String regionName, final URI endpoint) {
-        this.regionName = regionName;
-        this.endpoint = endpoint;
-    }
+	Region(final String regionName, final URI endpoint) {
+		this.regionName = regionName;
+		this.endpoint = endpoint;
+		regionsByName.put(regionName, this);
+	}
 
-    public String getRegionName() {
-        return regionName;
-    }
+	public String getRegionName() {
+		return regionName;
+	}
 
-    public URI getEndpoint() {
-        return endpoint;
-    }
+	public URI getEndpoint() {
+		return endpoint;
+	}
 
-    public static Region fromString(final String regionName) {
-        return Stream.of(values())
-            .filter(e -> e.regionName.equals(regionName))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("No matching constant for: " + regionName));
-    }
+	public static Region fromString(final String regionName) {
+		Region r = regionsByName.get(regionName);
+		if (r == null) {
+			// Assume it might be endpoint URL for custom endpoint
+			try {
+				URI ep = create(regionName);
+				r = new Region(regionName, ep);
+			} catch (IllegalArgumentException iax) {
+				throw new IllegalArgumentException("No matching region and not valid URL: " + regionName);
+			}
+		}
+		return r;
+	}
 
-    @Override
-    public String toString() {
-        return "Region{" +
-            "regionName='" + regionName + '\'' +
-            ", endpoint=" + endpoint +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "Region{" + "regionName='" + regionName + '\'' + ", endpoint=" + endpoint + '}';
+	}
 }
